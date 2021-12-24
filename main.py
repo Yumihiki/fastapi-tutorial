43080a8012b0c04c1af9b988885b8edf241a3cc6
nibupro <nibu10.majik@gmail.com>
2021-12-24 23:26:13 +0900

    FastAPIチュートリアル最初のステップ終了

--- main.py
+++ main.py
@@ -1,35 +1,8 @@
-from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-def get_full_name(first_name: str, last_name: str):
-    full_name = first_name.title() + " " + last_name.title()
-    return full_name
-
-
-def get_name_with_age(name: str, age: int):
-    name_with_age = name + " is this old: " + str(age)
-    return name_with_age
-
-
-def get_items(item_a: str, item_b: int, item_c: float, item_d: bool, item_e: bytes):
-    """ strだけでなく、全てのPython標準型を宣言できます。"""
-    return item_a, item_b, item_c, item_d, item_d, item_e
-
-
-def say_hi(name: Optional[str] = None):
-    if name is not None:
-        print(f"Hey {name}!")
-    else:
-        print("Hello World")
-
-
-class Person:
-    def __init__(self, name: str):
-        self.name = name
-
-
-def get_person_name(one_person: Person):
-    return one_person.name
-
-
-print(get_full_name("john", "doe"))
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
