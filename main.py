77e893b93cfe092f7e32e2106f69543b3958bc0b
nibupro <nibu10.majik@gmail.com>
2021-12-25 20:44:25 +0900

    FastAPIチュートリアル

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from datetime import HTML
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/items/")
+def read_items(q: str = Query("fixedquery", min_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
